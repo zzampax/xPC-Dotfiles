19771331a2bf3a343b9ed8012683c49c2b950710
zzampax <zpx171997@gmail.com>
2024-10-02 22:58:25 +0200

    Update FXArch Dotfiles

--- src/FXArch/fish/config.fish
+++ src/FXArch/fish/config.fish
@@ -4,7 +4,7 @@ end
 set fish_greeting
 function fish_prompt
     # Start with normal color
-    echo -n (printf '\033[38;2;180;190;254m')
+	echo -n (printf '\033[38;2;235;160;172m')
     # Print the user@host:pwd$ prompt
     echo -n "["(whoami)"@"(cat /etc/hostname)":"(prompt_pwd)"\$] "
     # Reset the color
@@ -22,12 +22,25 @@ end
 
 bind \cl ''
 
+function ssh
+    # Check if we are in the kitty terminal
+    if test "$TERM" = "xterm-kitty"
+        # Connect via SSH and immediately start tmux on the remote machine
+        command tmux new-session "ssh $argv; exit"
+    else
+        # Just run the regular ssh command if not in kitty
+        command ssh $argv
+    end
+end
+
 # Aliases
 alias g='git'
+alias gback='git commit -m "Backup $(date +"%Y-%m-%d %H:%M:%S")"'
 alias ls='ls --group-directories-first --color=auto'
 alias ll='ls -l'
 alias l='ls -lA'
 alias ip='ip -c'
+alias tree='tree -C --dirsfirst'
 alias fetch='clear && fastfetch'
 alias sys='fastfetch'
 alias localvenv='source .venv/bin/activate.fish'
